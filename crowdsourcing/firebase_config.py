ca611325507c7f87bdd24a8e98a6175e7af94dd2
Arnav Sharma <arnavsharma.0914@gmail.com>
2024-07-17 12:50:06 -0400

    A little UI styling js waiting on you Vib for the rest as I also added TailwindCSS to hello.html

--- crowdsourcing/firebase_config.py
+++ crowdsourcing/firebase_config.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate(r"C:\Users\HP\Desktop\Coding\Python\Django\straibismus\StrabinetAuthKey.json") #Arnav & Vib, edit this path to the path of the auth key I sent you on your system
+cred = credentials.Certificate(r"C:\Users\arnav\Downloads\StrabinetAuthKey.json") #Arnav & Vib, edit this path to the path of the auth key I sent you on your system
 firebase_admin.initialize_app(cred)
